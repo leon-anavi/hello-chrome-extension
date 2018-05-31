722c0b5b2cb758e077c66ab4e271a49135a995c6
Leon Anavi <leon@anavi.org>
2018-05-31 20:03:03 +0300

    Support two colors in the popup
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+  chrome.storage.sync.set({color: '#3aa757' }, function() {
+    console.log("Primary color is green");
+  });
+  chrome.storage.sync.set({color2: '#FF0000' }, function() {
+    console.log("Secondary color is red");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
--- popup.js
+++ popup.js
@@ -1,11 +1,27 @@
 let changeColor = document.getElementById('changeColor');
+let changeColor2 = document.getElementById('changeColor2');
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
 
+chrome.storage.sync.get('color2', function(data) {
+  changeColor2.style.backgroundColor = data.color2;
+  changeColor2.setAttribute('value', data.color2);
+});
+
 changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+
+changeColor2.onclick = function(element) {
+  console.log("test");
   let color = element.target.value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
